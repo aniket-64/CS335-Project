f39ab7bd520f4e555d004c872bb82b57721daf89
aniket-64 <96648292+aniket-64@users.noreply.github.com>
2024-03-31 19:16:43 +0530

    Milestone 2 added

---
+++ milestone2/scripts/test.sh
@@ -0,0 +1,26 @@
+search_and_parse() {
+    local input_dir="$1"
+    local output_dir="$2"
+    local parser="../src/parser -input"
+    
+    # Create the output directory structure if it doesn't exist
+    mkdir -p "$output_dir${input_dir#$1}"
+
+    for item in "$input_dir"/*; do
+        if [ -f "$item" ] && [ "${item##*.}" = "py" ]; then
+            echo
+            echo "Testing $item"
+	    output_file="${item/#$1/$2}"
+            output_file="${output_file%.py}.out"
+            $parser "$item" "-output" "$output_file"
+            echo "Made $output_file"
+            echo
+        fi
+    done
+}
+
+cd ../src
+make clean
+make
+search_and_parse "../tests" "../out"
+make clean
